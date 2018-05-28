68fc99915dcbdb180620a19ecabea4c17bef4cb9
JKasyan <j.kasyan86@gmail.com>
2018-05-28 23:53:50 +0300

    sorted search

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,5 +1,8 @@
+import sys
+
+
 def merge_sort(ar, p, r):
-    q = (p + r) / 2
+    q = (p + r) // 2
     if p < r:
         merge_sort(ar, p, q)
         merge_sort(ar, q + 1, r)
@@ -9,21 +12,20 @@ def merge_sort(ar, p, r):
 def merge(ar, p, q, r):
     left = ar[p:q + 1]
     right = ar[q + 1:r + 1]
-    left.append(None)
-    right.append(None)
-    i = 0  # left
-    j = 0  # right
-    ran = range(p, r + 1)
-    for k in ran:
-        print('k ~ ', k)
+    left.append(sys.maxsize)
+    right.append(sys.maxsize)
+    i, j = 0, 0
+    k = p
+    while k <= r:
         if left[i] <= right[j]:
             ar[k] = left[i]
             i += 1
         else:
             ar[k] = right[j]
             j += 1
+        k += 1
 
 
-array = [4, 2, 1, 3]
-merge_sort(array, 0, 3)
+array = [4, 2, 3, 1, 0, 6, 7]
+merge_sort(array, 0, len(array) - 1)
 print(array)
---
+++ sorting/sorted_search.py
@@ -0,0 +1,27 @@
+import math
+ar = list(range(0, 1000000))
+
+
+def binary_search(array, value):
+    searched_position = -1
+    current_pos = len(array) - 1
+    min_pos = 0
+    max_pos = len(array) - 1
+    n = 0
+    while True:
+        if array[current_pos] == value:
+            searched_position = current_pos
+            break
+        elif value > array[current_pos]:
+            min_pos = current_pos
+        else:
+            max_pos = current_pos
+        current_pos = (min_pos + max_pos) // 2
+        n += 1
+    print('n ~ ', n)
+    return searched_position
+
+
+print('log2n ~ ', math.log(len(ar), 2))
+pos = binary_search(ar, 800120)
+print(pos)
